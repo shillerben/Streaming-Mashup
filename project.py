d6e0166fc2a32ba6c2fc365966036cdf5fe81fb8
Matthew Mora <mattmora44@tamu.edu>
2019-05-02 18:43:07 -0500

    revert

--- project.py
+++ project.py
@@ -70,7 +70,7 @@ def result(movie):
 
 
 
-    return render_template('result.html', title=movie_title, poster_url=poster_url, locations=locations, year=year, rating = movie_rating, runtime = movie_runtime, plot = movie_plot, genre = movie_genre, reviews = movie_reviews, actors = movie_actors, website = movie_website, director = movie_director, walmart = walmartLink, bestBuy = BBLink, amazon = amazonLink)
+    return render_template('result.html', title=movie_title, poster_url=poster_url, locations=locations, year=year, rating = movie_rating, runtime = movie_runtime, plot = movie_plot, genre = movie_genre, reviews = movie_reviews, actors = movie_actors, website = movie_website, director = movie_director)#, walmart = walmartLink, bestBuy = BBLink, amazon = amazonLink)
 
 
 
